952d6f617895cb3fac490a77c21d177f5b7ea766
Ilya Kunitsky <ilyhhaa@gmail.com>
2022-01-13 14:36:46 +0300

    Add files via upload

---
+++ ExceptionClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Exception
+{
+    public class ExceptionClass
+    {
+        private int age;
+        public string Name { get; set; }
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value < 18)
+                {
+                    throw new System.Exception("Людям не достигшим 18 летнего возраста регистрация запрещена");
+
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Имя пользователя : {Name} , Возраст пользователя : {age}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///////////////////////////////////////////
+            ///////////////FormatExeption//////////////
+            ///////////////////////////////////////////
+
+            try
+            {
+                Console.WriteLine("Введите первое слогаемое");/////////Намеренно вводим строку или символ
+                int Input = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите второе слогаемое"); /////////Намеренно вводим строку или символ
+                int Input1 = int.Parse(Console.ReadLine());
+
+                int Add = Input + Input1;
+                Console.WriteLine(Add);
+
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.TargetSite);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            ///////////////////////////////////////////
+            ///////////////Throw///////////////////////
+            ///////////////////////////////////////////
+            Console.WriteLine("Введите имя пользователя");
+            string? name = Console.ReadLine();
+            try
+            {
+                if (name == null || name.Length <= 3)
+                {
+                    throw new System.Exception ("Длина имени должна быть больше трех");
+                }
+                else
+                {
+                    Console.WriteLine(name);
+                }
+                
+               
+            }
+            catch (SystemException e)
+            {
+                Console.WriteLine("Ошибка : " + e.Message);
+                throw;
+            }
+
+            ///////////////////////////////////////////
+            ///////////////ClassesException////////////
+            ///////////////////////////////////////////
+            ExceptionClass exception = new ExceptionClass(){ Age = 12, Name = "Ilya" };
+            exception.Display();
+        }
+
+
+
+
+
+
+    }
+}
